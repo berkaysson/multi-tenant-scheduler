72844a775f6fe3225f5e0ceed0dae66bc965b7fd
berkay <berkaysonel85@gmail.com>
2025-10-12 15:00:15 +0300

    fix: starter pack route error and middleware infinite route redirection error

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,5 +1,5 @@
-import GitHub from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+// import GitHub from "next-auth/providers/github";
+// import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "./schemas";
@@ -20,14 +20,14 @@ import bcrypt from "bcryptjs";
 
 export default {
   providers: [
-    GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    // GitHub({
+    //   clientId: process.env.GITHUB_CLIENT_ID,
+    //   clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    // }),
+    // Google({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
